e131222a2fe81a56dbd6d399b38abcf55286c188
kc0506 <kchong0506@gmail.com>
2024-09-15 14:49:23 +0800

    Add ac_code1.c for quiz_new

---
+++ quiz_new/solutions/ac_code1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define MAX_N 400000
+#define MAX_BITS 32
+
+unsigned int arr[MAX_N];
+
+void print_bins(unsigned int x) {
+    for (int i = 0; i < MAX_BITS; i++) {
+        printf("%d", (x >> (MAX_BITS - 1 - i)) & 1);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n, k;
+
+    scanf("%d %d", &n, &k);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%u", &arr[i]);
+    }
+
+    unsigned int mask_unit = (1 << k) - 1;
+    unsigned int mask_low = 0, mask_high = 0;
+
+    for (int i = 0; 2 * i * k < MAX_BITS; i++) mask_low |= mask_unit << 2 * i * k;
+    for (int i = 0; (2 * i + 1) * k < MAX_BITS; i++) mask_high |= mask_unit << (2 * i + 1) * k;
+
+    if (!((mask_low | mask_high) == ~0u && (mask_low & mask_high) == 0)) {
+        printf("something wrong with the mask\n");
+        return -1;
+    };
+
+    for (int i = 0; i < n; i++) {
+        unsigned int res = ((arr[i] & mask_high) >> k) | ((arr[i] & mask_low) << k);
+        printf("%u%c", res, " \n"[i == n - 1]);
+    }
+}
