0aba0e6ebdfad1f7422bd4de2d2b57ec37ef09a2
kc0506 <kchong0506@gmail.com>
2024-09-15 23:19:31 +0800

    Copy AC_Code.c to completely follow folder structure showed  in the slides 🙃

---
+++ quiz/AC_Code.c
@@ -0,0 +1,60 @@
+/**
+ * ? This file is the same as `solutions/AC1.c`,
+ * ? but we copied it here to avoid any chances of
+ * ? getting a deduction for wrong directory structure :)
+ */
+// :) Solving with bit masking
+
+#include <stdio.h>
+
+#define MAX_N 400000
+#define MAX_BITS 32
+
+unsigned int arr[MAX_N];
+
+// For debugging
+void print_bins(unsigned int x) {
+    for (int i = 0; i < MAX_BITS; i++) {
+        printf("%d", (x >> (MAX_BITS - 1 - i)) & 1);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n, k;
+
+    scanf("%d %d", &n, &k);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%u", &arr[i]);
+    }
+
+    if (k == 0) {
+        for (int i = 0; i < n; i++) {
+            printf("%u%c", arr[i], " \n"[i == n - 1]);
+        }
+        return 0;
+    }
+    if (k == 32) {
+        for (int i = 0; i < n; i++) {
+            printf("0%c", " \n"[i == n - 1]);
+        }
+        return 0;
+    }
+
+    unsigned int mask_unit = (1 << k) - 1;
+    unsigned int mask_low = 0, mask_high = 0;
+
+    for (int i = 0; 2 * i * k < MAX_BITS; i++) mask_low |= mask_unit << 2 * i * k;
+    for (int i = 0; (2 * i + 1) * k < MAX_BITS; i++) mask_high |= mask_unit << (2 * i + 1) * k;
+
+    if (!((mask_low | mask_high) == ~0u && (mask_low & mask_high) == 0)) {
+        printf("something wrong with the mask\n");
+        return -1;
+    };
+
+    for (int i = 0; i < n; i++) {
+        unsigned int res = ((arr[i] & mask_high) >> k) | ((arr[i] & mask_low) << k);
+        printf("%u%c", res, " \n"[i == n - 1]);
+    }
+}
